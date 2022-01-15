ac1909daf6b6cf68935e1e1a9d85b6e598ac1351
neeejm <nejmzakaria@gmail.com>
2022-01-15 16:15:01 +0100

    adding image background

--- frontend/src/pages/home_page/Home.js
+++ frontend/src/pages/home_page/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Button, Center, Container, FormControl, HStack, Image, Input } from "@chakra-ui/react";
+import { Box, Button, Center, Container, FormControl, HStack, Image, Input, VStack, Text } from "@chakra-ui/react";
 import Navbar from "../../components/Navbar/Navbar";
 import "./home.css"
 
@@ -8,24 +8,54 @@ const ENCODE = "data:image/png;base64, "
 let isBuffer = false
 
 const Home = () => {
-    const [imgSrc, setImgSrc] = useState("");
+    const [imgSrc, setImgSrc] = useState("https://t3.ftcdn.net/jpg/02/57/43/20/360_F_257432094_IBWsGRGo9DXMS9glqquVlp3QQOly2UZA.jpg");
+    const [faces, setFaces] = useState([]);
+    const [isLoading, setIsLoading] = useState(null);
 
     const img = (() => {
         if (imgSrc !== "") {
             console.log("buffer: ", isBuffer)
             if (isBuffer) {
-                return (
-                    <Center>
-                        <Image id="face" src={ENCODE + imgSrc} maxW="550px" maxH="550px" paddingBottom="30px" />
-                        <div className="bounding-box">
+                if (isLoading)
+                    return (
+                        <Center>
+                            <div className="loader"></div>
+                        </Center>
+                    );
+                else
+                    return (
+                        <div>
+                            <Center>
+                                <Box>
+                                    <Image id="face" src={ENCODE + imgSrc} maxW="550px" maxH="550px" />
+                                    <div className="bounding-box">
+                                    </div>
+                                </Box>
+                            </Center>
+                            <Box padding="20px">
+                                <VStack maxH="100px" overflowX="auto">
+                                    {faces.map((face, index) => {
+                                        return (
+                                            <div>
+                                                <HStack>
+                                                    <Image id={index} src={ENCODE + face.image} maxW="100px" maxH="100px" />
+                                                    <Text fontsize="md">
+                                                        {face.gender ? ((face.name === "") ? "John Doe" : face.name) + " (male)" :
+                                                            ((face.name === "") ? "Jane Doe" : face.name) + " (female)"}
+                                                    </Text>
+                                                </HStack>
+                                            </div>
+                                        );
+                                    })}
+                                </VStack>
+                            </Box>
                         </div>
-                    </Center>
-                );
+                    );
             }
             else {
                 return (
                     <Center>
-                        <Image id="face" src={IMAGE_PROXY_URL + imgSrc} maxW="550px" maxH="550px" paddingBottom="30px" />
+                        <Image id="face" src={IMAGE_PROXY_URL + imgSrc} maxW="550px" maxH="550px" />
                         <div className="bounding-box">
                         </div>
                     </Center>
@@ -36,41 +66,53 @@ const Home = () => {
     })();
 
     const handleImg = (event) => {
-        try {
-            // new URL(event.target.value);
+        if (event.target.value === "")
+            setImgSrc("https://t3.ftcdn.net/jpg/02/57/43/20/360_F_257432094_IBWsGRGo9DXMS9glqquVlp3QQOly2UZA.jpg")
+        else {
             setImgSrc(event.target.value);
             isBuffer = false
-        }
-        catch (err) {
-            console.log(err);
         }
     }
 
     const detectFace = () => {
-        isBuffer = true
-        fetch("http://127.0.0.1:3003/drawbox?url=" + imgSrc, {
-            // mode: "no-cors",
-            method: 'POST'
-        })
-            .then(response => response.json())
-            // .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
-            .then(result => setImgSrc(result.image.url))
-            // .then(result => console.log(result))
-            .catch(error => console.log('error', error));
+        try {
+            new URL(imgSrc);
+            setIsLoading(true);
+            isBuffer = true;
+            fetch("http://127.0.0.1:3003/detect?url=" + imgSrc, {
+                // mode: "no-cors",
+                method: 'POST'
+            })
+                .then(response => response.json())
+                // .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+                .then(result => {
+                    setIsLoading(false);
+                    // setFaces(result.images.faces)
+                    setImgSrc(result.images.image_box);
+                })
+                // .then(result => console.log(result))
+                .catch(error => console.log('error', error));
+        }
+        catch (err) {
+            console.error("not a url");
+            return;
+        }
     }
 
     return (
         <div>
             <Navbar />
             <Container paddingTop="50px" maxW="container.sm">
-                {img}
+                <Box borderWidth='4px' borderRadius='lg' overflow='hidden' padding="30px" marginBottom="20px" borderColor="#2e479c">
+                    {img}
+                </Box>
                 <FormControl>
                     <Box >
                         <div style={{ paddingBottom: "100px" }}>
                             <HStack>
-                                <Input placeholder='image of the face to find...' onChange={handleImg} />
-                                <Button colorScheme="yellow" onClick={detectFace}>detect</Button>
-                                <Button colorScheme="red">find</Button>
+                                <Input placeholder='image of the face to find...' onChange={handleImg} borderColor="#2e479c" borderWidth="2px" />
+                                <Button textTransform="uppercase" colorScheme="yellow" onClick={detectFace}>detect</Button>
+                                {/* <Button colorScheme="red">find</Button> */}
                             </HStack>
                         </div>
                     </Box>
